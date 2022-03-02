d5830c9e1c89da1dca73b72be44075fdbdee23ed
Nour <nour@Ayas-MBP-2.lan>
2022-03-02 21:45:59 +0300

    Finished Task Day 1

---
+++ cw-day1/cw-day1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw-day1
+//
+//  Created by Nour on 3/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hell my name is Nour!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw-day1/cw-day1/cw_day1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw_day1App.swift
+//  cw-day1
+//
+//  Created by Nour on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct cw_day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
